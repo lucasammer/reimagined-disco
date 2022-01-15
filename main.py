5ca6f51553cf0815d0fcee7620edb04637ceab14
lucasammer <lucasammerlaan123@gmail.com>
2022-01-15 14:23:37 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import handydandy
-handydandy.colors.replonly.printc('hello world!', handydandy.colors.OKCYAN)
+import random
+import time
+
+while True:
+    colors = [handydandy.colors.HEADER, handydandy.colors.OKBLUE ,handydandy.colors.OKCYAN, handydandy.colors.OKGREEN, handydandy.colors.WARNING, handydandy.colors.FAIL, handydandy.colors.BOLD, handydandy.colors.UNDERLINE]
+    color = random.choice(colors)
+    words = ['progressive', 'door', 'seminar', 'mold', 'disco', 'disco', 'contrast', 'foreigner', 'disco', 'plain', 'magnetic', 'cathedral', 'unity', 'disco', 'disco']
+    word = random.choice(words)
+    time.sleep(.1)
+    handydandy.colors.replonly.printc(word, color)
